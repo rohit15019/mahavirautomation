7eeba4353bc8978e98128fa3a079a187c03325d6
rohit15019 <rohitparmar15019@gmail.com>
2026-07-09 17:38:43 +0530

    Increase Express JSON body size limits to 50mb

--- back-end/server.js
+++ back-end/server.js
@@ -17,8 +17,8 @@ app.use(cors({
 }));
 
 // Body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve Uploads as Static files
 // Ensure files stored in back-end/uploads are served at /uploads
